d845f948f1f8ba9ada979c65b38863d55634d5b3
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-24 21:35:04 +0300

    Add project files.

---
+++ ExVariablesExpressionsInputData/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ExVariablesExpressionsInputData
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputDataA = Console.ReadLine();
+            int a = Convert.ToInt32(inputDataA);
+
+            string inputDataB = Console.ReadLine();
+            int b = Convert.ToInt32(inputDataB);
+
+            Console.WriteLine(a+b);
+            Console.Read();
+       
+        }
+    }
+}
