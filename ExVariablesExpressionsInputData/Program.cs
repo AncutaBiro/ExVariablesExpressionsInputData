22025d4203288ef98af1cc9f8a83bf5edb320697
AncutaBiro <ancuta.biro@yahoo.com>
2020-09-26 16:20:20 +0300

    Test 3 problem 1

--- ExVariablesExpressionsInputData/Program.cs
+++ ExVariablesExpressionsInputData/Program.cs
@@ -1,4 +1,12 @@
-﻿// check if a number is positive or negative
+﻿//După ploaie au răsărit mai multe ciuperci roșii și albe. Cele roșii sunt de X ori mai multe decât cele albe.
+//Scrie o aplicație ce primește ca date de intrare numărul total de ciuperci și numărul X și întoarce numărul de ciuperci roși.
+//Datele de intrare se vor citi de la tastatură, câte un număr pe linie.
+//Exemplu:
+//Pentru datele de intrare:
+//10
+//4
+//La consolă se va afișa:
+//8
 
 using System;
 
@@ -8,24 +16,17 @@ namespace ExVariablesExpressionsInputData
     {
         static void Main(string[] args)
         {
-            string inputDataA = Console.ReadLine();
-            int a = Convert.ToInt32(inputDataA);
+            string inputData = Console.ReadLine();
+            int totalMushrooms = Convert.ToInt32(inputData);
 
-            if (a == 0)
-            {
-                Console.WriteLine("0");
-                Console.Read();
-            }
-            else if (a > 0)
-            {
-                Console.WriteLine("True");
-                Console.Read();
-            }
-            else if (a < 0)
-            {
-                Console.WriteLine("False");
-                Console.Read();
-            }
+            inputData = Console.ReadLine();
+            int timesWhiteMushrooms = Convert.ToInt32(inputData);
+
+            int whiteMushrooms = totalMushrooms / (1 + timesWhiteMushrooms);
+            int redMushrooms = timesWhiteMushrooms * whiteMushrooms;
+
+            Console.WriteLine(redMushrooms);
+            Console.Read();
+        }
         }
     }
-}
